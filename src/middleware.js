8c405b7047356b101ed5ae03cc3a6f4b5abfeae4
Masrafi404 <mdmasrafi902@gmail.com>
2025-03-05 06:18:19 +0600

    learn nextjs middleware

---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  let cookie = request.cookies.get("nextjs-cookie");
+  console.log(cookie.value);
+
+  const featuresPage = request.nextUrl.pathname.startsWith("/features");
+  const aboutPage = request.nextUrl.pathname.startsWith("/About:/path");
+
+  const isAdmin = true;
+  const isEmail = true;
+
+  if (featuresPage && !isEmail) {
+    return NextResponse.rewrite(new URL("/login", request.url));
+  }
+
+  if (aboutPage && !isAdmin) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
